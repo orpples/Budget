bd6cfcf047f08c4e3bf35b4808e0c2f0016cc20e
orpples <orpples594@gmail.com>
2022-09-12 20:16:03 -0700

    added prompts to ask questions on weekly/monthly/annual expenses

--- index.js
+++ index.js
@@ -2,4 +2,40 @@ let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
 
-// Your Code Here
+for (let i = 0; i < weeklyExpenseQuestions.length; i++) {
+    console.log(i);
+    console.log(weeklyExpenseQuestions[i]);
+    //Get user response to question
+    let stringAnswer = window.prompt(weeklyExpenseQuestions[i]);
+    console.log(stringAnswer);
+    //Convert sting to number for calculations
+    let numberAnswer = parseFloat(stringAnswer)
+    //Add numberAnswer to expenses total variable
+    weeklyExpenses = weeklyExpenses + numberAnswer
+}
+
+//monthly expenses
+for (let i = 0; i < monthlyExpenseQuestions.length; i++) {
+    console.log(i);
+    console.log(monthlyExpenseQuestions[i]);
+    //Get user response to question
+    let stringAnswer = window.prompt(monthlyExpenseQuestions[i]);
+    console.log(stringAnswer);
+    //Convert sting to number for calculations
+    let numberAnswer = parseFloat(stringAnswer)
+    //Add numberAnswer to expenses total variable
+    monthlyExpenses = monthlyExpenses + numberAnswer
+}
+
+//annual expenses
+for ( let i=0 ; i < annualExpenseQuestions.length ; i++ ) {
+    console.log(i);
+    console.log(annualExpenseQuestions[i]);
+    //get user response to question
+    let stringAnswer = window.prompt(annualExpenseQuestions[i])
+    console.log(stringAnswer);
+    //Convert sting to number for calculations
+    let numberAnswer = parseFloat(stringAnswer);
+    //add numberAnswer to expenses total variable
+    monthlyExpenses = monthlyExpenses + numberAnswer
+}
